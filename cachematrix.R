4b81d33f2fbc5288b38f7c7f727a50904d5ac312
datascimjk <ccmanip@hotmail.com>
2014-05-25 18:25:42 -0400

    added assignment examplas as template

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,39 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
+
+
+
+
+
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+cacheSolve <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
